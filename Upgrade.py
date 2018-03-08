949fa6dda2bf419b20830033cd65ab2c53dade20
Static0verride <ohmnoo@gmail.com>
2018-03-07 23:07:03 -0800

    Add files via upload

--- Upgrade.py
+++ Upgrade.py
@@ -1,4 +1,5 @@
 import telnetlib
+import sys
 from commands import *
 
 
@@ -24,7 +25,46 @@ class FastIron:
         for val in my_list:
             self.session.write(val)
 
+    @staticmethod
+    def __creates_list_of_nlines(my_string):
+        """ Breaks a long string into separated substring"""
+        temp = ""                       # sets empty string, will add char respectively
+        my_list = list()                # creates list
+        for val in range(0, len(my_string)):    # iterates through the length of input
+            if my_string[val] == '\n' and temp == "":
+                continue
+            elif my_string[val] == '\n' or val >= len(my_string):    # add what was found
+                my_list.append(temp)
+                temp = ""
+            else:
+                if my_string[val] == ' ' and my_string[val+1] == '\n':
+                    continue
+                temp += my_string[val]
+
+        return my_list
+
     def show_version(self):
         self.session.write(b"show version \n")
         return self.session.read_until(b'HW: ', 1).decode('utf-8')
 
+    def show_ip_bindings(self):
+        self.session.write(b"show ip dhcp-server binding \n")
+        return self.session.read_until(b'None just space', 1).decode('utf-8')
+
+    def obtain_binding(self):
+        find_word = "Hardware address"
+        output = self.show_ip_bindings()
+        data_dic = dict()
+        token = output.find(find_word) + len(find_word) + 1
+        nlines = FastIron.__creates_list_of_nlines(output[token:len(output)])
+
+        for data in nlines:
+
+            if len(data) <= 1:
+                continue
+            if 'Telnet' in data or 'Switch' in data:
+                continue
+
+            ip, mac, __, __ = data.split()
+            data_dic.update({mac: {'ip': ip, 'serial': None}})
+        return data_dic
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from Upgrade import *
 import time
 
-Driver = FastIron('10.176.217.151')
 
-Driver.open()
-print(Driver.show_version())
-Driver.close()
+""" Connect to DHCP Server and obtain bindings"""
+DHCP_Server = FastIron('10.176.217.148')
+DHCP_Server.open()
+print(DHCP_Server.obtain_binding())
+DHCP_Server.close()
+"""Connect to devices using IP bindings obtained"""
+
+"""Push default security configuration"""
+
+"""Grab Actual security configuration"""
+
+"""Map serial # to configuration and push respective config"""
+
 
